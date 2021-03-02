5ca617d147c54e203b0ec526bc6e63502f3f3c4a
yating <yangyating@irigel.com>
2021-03-02 21:02:12 +0800

    编写登陆页面

---
+++ app/src/main/java/com/example/zerolab/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.zerolab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_activity_signin);
+    }
+}
